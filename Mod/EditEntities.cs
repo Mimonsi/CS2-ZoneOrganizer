15190299cb774899d1331fb96a8d3feef0a23b84
kosch104 <konstantin-schwarze@outlook.de>
2024-12-06 02:13:56 +0100

    Remove unneeded stuff

--- Mod/EditEntities.cs
+++ Mod/EditEntities.cs
@@ -79,6 +79,7 @@ namespace ZoneOrganizer.Mod
 			PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Zones", "Low Density Residential","Media/Game/Icons/ZoneResidentialLow.svg");
 			PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Zones", "Medium Density Residential", "Media/Game/Icons/ZoneResidentialMedium.svg", "Low Density Residential");
 			PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Zones", "High Density Residential", "Media/Game/Icons/ZoneResidentialHigh.svg", "Medium Density Residential");
+			PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Zones", "Mixed Housing", "Media/Game/Icons/ZoneResidentialMixed.svg", "High Density Residential");
 			foreach (Entity entity in entities)
 			{
 				ExtraLib.m_PrefabSystem.TryGetPrefab(entity, out ZonePrefab zonePrefab);
@@ -97,10 +98,13 @@ namespace ZoneOrganizer.Mod
 				if (zonePrefab.name.Contains("Residential"))
 				{
 					// Zone name filter heavily inspired by Find It by T.D.W. (https://github.com/JadHajjar/FindIt-CSII)
-					if (zonePrefab.name.Contains(" Row") || zonePrefab.name.Contains(" Medium") || zonePrefab.name.Contains(" Mixed"))
+					if (zonePrefab.name.Contains(" Mixed"))
+					{
+						prefabUI.m_Group = PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Zones", "Mixed Housing", "Media/Game/Icons/ZoneResidentialMixed.svg", "High Density Residential");
+					}
+					if (zonePrefab.name.Contains(" Row") || zonePrefab.name.Contains(" Medium"))
 					{
 						prefabUI.m_Group = PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Zones", "High Density Residential", "Media/Game/Icons/ZoneResidentialHigh.svg", "Medium Density Residential");
-
 					}
 					else if (zonePrefab.name.Contains(" High") || zonePrefab.name.Contains(" LowRent"))
 					{
@@ -109,7 +113,6 @@ namespace ZoneOrganizer.Mod
 					else if (zonePrefab.name.Contains(" Low"))
 					{
 						prefabUI.m_Group = PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Zones", "Low Density Residential", "Media/Game/Icons/ZoneResidentialLow.svg");
-						prefabUI.m_Icon = "Media/Game/Icons/ZoneResidentialLow.svg";
 					}
 				}
 				/*else
@@ -121,8 +124,6 @@ namespace ZoneOrganizer.Mod
 
 				ExtraLib.m_EntityManager.AddOrSetComponentData(entity, prefabUI.ToComponentData());
 			}
-
-			Log("Marker Object Entities Edited.");
 		}
 
 	}
